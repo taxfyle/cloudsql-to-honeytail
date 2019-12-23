31ed70dab87bff1435ddcba769f2c4aa9e862912
Paul Daniel Faria <Nashenas88@users.noreply.github.com>
2019-12-23 15:00:14 -0500

    Add ability to specify additional log data to honeytail

--- run.sh
+++ run.sh
@@ -7,6 +7,16 @@ if ! [[ -z "${DEBUG:-}" ]]; then
     HTDEBUG="--debug --debug_stdout"
 fi
 
+if ! [[ -z "${ADD_PARAMS:-}" ]]; then
+    ADD_PARAMS_ARR=($(echo "${ADD_PARAMS}"));
+    ADDITIONAL_FLAGS=""
+    for (( i = 1 ; i < ${#ADD_PARAMS_ARR[@]} ; i+=2 )); do
+        FIELD_NAME="${ADD_PARAMS_ARR[$i]}"
+        FIELD_VALUE="${ADD_PARAMS_ARR[$((i + 1))]}"
+        ADDITIONAL_FLAGS+="--add_field \"${FIELD_NAME}\"=\"${FIELD_VALUE}\" "
+    done
+fi
+
 # In-cluster gcloud auth
 if [[ "${GOOGLE_APPLICATION_CREDENTIALS_JSON:-}" != "" ]]; then
     echo $GOOGLE_APPLICATION_CREDENTIALS_JSON > service-account.key
@@ -19,4 +29,5 @@ fi
     --dataset="${DATASET:-postgres}" \
     --parser=postgresql \
     --postgresql.log_line_prefix='[%t]: [%p]: [%l-1] db=%d,user=%u' \
+    "${ADDITIONAL_FLAGS:-}" \
     -f -
