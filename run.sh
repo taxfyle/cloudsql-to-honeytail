1cfbbed167abfe6c7c6066f1a28f16fbc13d352d
Paul Daniel Faria <Nashenas88@users.noreply.github.com>
2019-12-26 15:10:22 -0500

    Add ability to specify additional log data to honeytail (#1)
    * Add ability to specify additional log data to honeytail
    
    * Allow env to pass additional fields to track, upgrade shell to bash to handle script
    
    * Add param count check so we fail early with helpful message

--- run.sh
+++ run.sh
@@ -7,6 +7,27 @@ if ! [[ -z "${DEBUG:-}" ]]; then
     HTDEBUG="--debug --debug_stdout"
 fi
 
+ADDITIONAL_FLAGS=()
+if ! [[ -z "${ADD_PARAMS:-}" ]]; then
+    ADD_PARAMS_ARR=()
+    while IFS='' read val; do
+        ADD_PARAMS_ARR+=("$val")
+    done < <(xargs -n1 <<<"$ADD_PARAMS")
+    if (( ${#ADD_PARAMS_ARR[@]} % 2 == 1 )); then
+        echo "Invalid number of additional parameters. Must be divisble by two."
+        echo "Please ensure you escape params with spaces with single or double"
+        echo "quotes and not with backslashes before the spaces."
+        exit 1
+    fi
+
+    for (( i = 0 ; i < ${#ADD_PARAMS_ARR[@]} ; i+=2 )); do
+        FIELD_NAME="${ADD_PARAMS_ARR[$i]}"
+        FIELD_VALUE="${ADD_PARAMS_ARR[$((i + 1))]}"
+        ADDITIONAL_FLAGS+=("--add_field")
+        ADDITIONAL_FLAGS+=("'${FIELD_NAME}'='${FIELD_VALUE}'")
+    done
+fi
+
 # In-cluster gcloud auth
 if [[ "${GOOGLE_APPLICATION_CREDENTIALS_JSON:-}" != "" ]]; then
     echo $GOOGLE_APPLICATION_CREDENTIALS_JSON > service-account.key
@@ -19,4 +40,5 @@ fi
     --dataset="${DATASET:-postgres}" \
     --parser=postgresql \
     --postgresql.log_line_prefix='[%t]: [%p]: [%l-1] db=%d,user=%u' \
+    "${ADDITIONAL_FLAGS[@]}" \
     -f -
